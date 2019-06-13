fcbc381af6a519d56742abce9eb486dc928eb1a6
Luís Fernando Guedes <lfernandoguedes@gmail.com>
2019-06-13 20:22:43 -0300

    Return predominant colors of image

--- get-color.js
+++ get-color.js
@@ -2,20 +2,27 @@ const path = require('path')
 const getColors = require('get-image-colors')
 const fs = require('fs')
 const rgbHex = require('rgb-hex')
-var colorThief = require('color-thief')
-colorThief = new colorThief()
-const _ = require('lodash')
+const ColorThief = require('color-thief')
+const colorThief = new ColorThief()
 
-const colors = async () => {
-  const badge = path.join(__dirname, 'badges/atletico-mg.png')
-
-  let rgbs1 = await getColors(badge)
-  let proof1 = rgbs1.map(color => color.hex().replace('#','')) 
-
-  let rgbs2 = colorThief.getPalette(fs.readFileSync(badge))
-  let proof2 = rgbs2.map(rgb => rgbHex(rgb.join(',')))
-
-  console.log(proof1, proof2)
+const getColorsByColorThief = (image) => {
+  let rgbs = colorThief.getPalette(fs.readFileSync(image))
+  return rgbs.map(rgb => rgbHex(rgb.join(',')))
 }
 
-colors()
+const getColorsByGetColors = async (image) => {
+  let rgbs = await getColors(image)
+  return rgbs.map(color => color.hex().replace('#',''))
+}
+
+const getImageColors = async () => {
+  const badge = path.join(__dirname, 'badges/atletico-pr.png')
+  let colorsByColorThief = getColorsByColorThief(badge)
+  let colorsByGetColors = await getColorsByGetColors(badge)
+
+
+  let predominantColors = colorsByColorThief.filter(val => colorsByGetColors.includes(val))
+  console.log(predominantColors)
+}
+
+module.exports = getImageColors
